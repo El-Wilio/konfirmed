9c3616906590bc58f6cc2ef8bc2f0e6cf7ac8fb7
Chris Barrieau <Chris.Barrieau@hotmail.com>
2014-09-22 12:13:28 -0400

    general update

---
+++ code/classes/Account.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Account {
+
+	function __construct() {
+		
+	}
+	
+	function login($user, $encPass) {
+		
+	}
+	
+	function register($params) {
+		
+	}
+	
+	function logout() {
+		
+	}
+	
+}
+
+?>
---
+++ code/classes/Artist.class.php
@@ -0,0 +1,49 @@
+<?php 
+
+class Profile {
+
+	private $id;
+	private $name;
+	private $categories;
+	private $profilePicture;
+	
+	
+	
+	function __construct() {
+			
+	}
+	
+	//use id?
+	function load($id) {
+		
+	}
+	
+	//Start Category Management
+	function addCategories($categories) {
+		
+	}
+	
+	function removeCategory($category) {
+		
+	}
+	
+	function replaceAllCategoriesWith($categories) {
+		
+	}
+	
+	function addSubcategories($category, $subcategories) {
+		
+	}
+	
+	function removeSubcategory($category, $subcategory) {
+		
+	}
+	
+	function replaceAllSubCategoriesWith($category, $subcategories) {
+		
+	}
+	//End Category Management
+	
+}
+
+?>
